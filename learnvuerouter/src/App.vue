01b9b02603d3d99491569d7790710378e14597db
jyq-jet <2357920704@qq.com>
2020-11-28 10:40:25 +0800

    vuerouter学习，第一次commit

---
+++ learnvuerouter/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <router-link to="/home">首页</router-link>
+    <router-link to="/about">关于</router-link>
+<!--    <button @click="homeClick">首页</button>-->
+<!--    <button @click="aboutClick">关于</button>-->
+    <router-link :to="'/user/' + userId">用户</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  methods: {
+    homeClick() {
+      this.$router.push('/home')
+    },
+    aboutClick() {
+      this.$router.push('/about')
+    }
+  },
+  data() {
+    return {
+      userId: 'lisi'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.active{
+  color: red
+}
+</style>
---
+++ learnvuerouter/src/components/User.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>我是用户界面</h2>
+    <p>用户界面，嘿嘿嘿</p>
+    <p>用户id：{{userId}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "User",
+  computed: {
+    userId() {
+      return this.$route.params.userId
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
